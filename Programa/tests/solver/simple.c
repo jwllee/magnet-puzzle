b53dc8f0a7b770f444f9c13d0db083aed6043a97
jonathan <jonathan.wailam.lee@gmail.com>
2018-10-02 19:33:27 -0300

    Test on basic magnet puzzle methods

--- Programa/tests/solver/simple.c
+++ Programa/tests/solver/simple.c
@@ -9,16 +9,77 @@
 
 int main(int argc, char *argv[])
 {
-    // make a magnet
-    Magnet * magnet = malloc(sizeof(Magnet));
-    magnet->valuePriority[0] = -1;
-    magnet->valuePriority[1] = -1;
-    magnet->valuePriority[2] = -1;
+   if (argc != 2)
+   {
+       printf("Usage: ./main [test.txt]\n");
+       return 0;
+   }
 
-    printf("Unassigned: %d\n", UNASSIGNED);
-    printf("Empty: %d\n", EMPTY);
-    printf("Positive: %d\n", POSITIVE);
-    printf("Negative: %d\n", NEGATIVE);
+   FILE *data = fopen(argv[1], "r");
 
-    free(magnet);
+   if (!data)
+   {
+       printf("Error opening the file!\n");
+       return 2;
+   }
+
+   int c, r;
+   fscanf(data, "%d %d", &c, &r);
+
+   // read constraints
+   int *row_p = malloc(r * sizeof(int));
+   int *row_n = malloc(r * sizeof(int));
+   int *col_p = malloc(c * sizeof(int));
+   int *col_n = malloc(c * sizeof(int));
+
+   // read column constraints
+   // positive then negative
+   for (int i = 0; i < c; ++i)
+       fscanf(data, " %d ", &col_p[i]);
+   for (int i = 0; i < c; ++i)
+       fscanf(data, " %d ", &col_n[i]);
+
+   // row constraints
+   for (int i = 0; i < r; ++i)
+       fscanf(data, " %d ", &row_p[i]);
+   for (int i = 0; i < r; ++i)
+       fscanf(data, " %d ", &row_n[i]);
+
+   // read the board
+   char **board = malloc(r * sizeof(char *));
+   char cell;
+   for (int i = 0; i < r; ++i)
+   {
+       board[i] = malloc(c * sizeof(char));
+
+       for (int j = 0; j < c; ++j)
+       {
+           fscanf(data, " %c ", &cell);
+           board[i][j] = cell;
+       }
+
+       // printf("row %d: %s\n", i, board[i]);
+   }
+
+   Puzzle *puzzle = puzzle_init(r, c, board, row_p, row_n, col_p, col_n);
+   print_puzzle(puzzle);
+
+   if (is_fulfilled(puzzle, false))
+       printf("\nPuzzle fulfilled: true\n");
+   else
+       printf("\nPuzzle fulfilled: false\n");
+
+
+   puzzle_destroy(puzzle);
+
+   for (int i = 0; i < r; ++i)
+        free(board[i]);
+   free(board);
+   free(row_p);
+   free(row_n);
+   free(col_p);
+   free(col_n);
+   fclose(data);
+
+   return 0;
 }
