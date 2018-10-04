5adb42a08e4e723e5a9c3956d452c707adc348f3
jonathan <jonathan.wailam.lee@gmail.com>
2018-10-04 17:44:41 -0300

    Updated experiment script

--- Programa/scripts/run.sh
+++ Programa/scripts/run.sh
@@ -16,7 +16,7 @@ N=6
 for i in $(seq 0 $N)
 do
   echo "Testing test_0$i.txt"
-  /usr/bin/time -v -a -o ${RESULTS_DIR}/time-0$i.txt ./solver ${TESTS_DIR}/test_0$i.txt >> ${RESULTS_DIR}/stdout.txt
+  /usr/bin/time -a -o ${RESULTS_DIR}/time-0$i.txt -f "Command: %C\n[mm:ss.ms]: %E" ./solver ${TESTS_DIR}/test_0$i.txt >> ${RESULTS_DIR}/stdout.txt
 
   mv output.txt ${RESULTS_DIR}/output-0$i.txt
 done
