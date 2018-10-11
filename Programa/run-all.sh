eb67feabc65d613f1cc64620868aeb681afcb921
jonathan <jonathan.wailam.lee@gmail.com>
2018-10-11 17:20:35 -0300

    Run all script to allow prune and heuristics be input arguments

--- Programa/run-all.sh
+++ Programa/run-all.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 TIME_FILE=$1
-TIME_LIMIT=60
+TIME_LIMIT=600
+PRUNE=$2
+HEURISTICS=$3
 
 for i in $(seq 1 6)
 do
@@ -10,21 +12,21 @@ do
   fi
 
   echo "o_test_0$i"
-  timeout ${TIME_LIMIT}s /usr/bin/time -f "time: %E" -a -o $TIME_FILE ./solver ./tests/deprecated/test_0$i.txt
+  timeout ${TIME_LIMIT}s /usr/bin/time -f "time: %E" -a -o $TIME_FILE ./solver ./tests/deprecated/test_0$i.txt $PRUNE $HEURISTICS
   echo ""
 done
 
 for i in $(seq 1 5)
 do
   echo "test_0$i"
-  timeout ${TIME_LIMIT}s /usr/bin/time -f "time: %E" -a -o $TIME_FILE ./solver ./tests/test_0$i.txt
+  timeout ${TIME_LIMIT}s /usr/bin/time -f "time: %E" -a -o $TIME_FILE ./solver ./tests/test_0$i.txt $PRUNE $HEURISTICS
   echo ""
 done
 
 for i in $(seq 6 9)
 do
   echo "test_0$i"
-  timeout ${TIME_LIMIT}s /usr/bin/time -f "time: %E" -a -o $TIME_FILE ./solver ./tests/Lunatic/test_0$i.txt
+  timeout ${TIME_LIMIT}s /usr/bin/time -f "time: %E" -a -o $TIME_FILE ./solver ./tests/Lunatic/test_0$i.txt $PRUNE $HEURISTICS
   echo ""
 done
 
