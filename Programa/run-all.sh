c0047ccf570949235f18b93533d78bf66d5da82a
jonathan <jonathan.wailam.lee@gmail.com>
2018-12-19 15:19:51 -0300

    Modified run shell script

--- Programa/run-all.sh
+++ Programa/run-all.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+TIME_LIMIT=60s
+
 for i in $(seq 1 6)
 do
   echo "deprecated test_0$i.txt"
-  /usr/bin/time -f "%E" ./solver ./tests/deprecated/test_0$i.txt
+  timeout $TIME_LIMIT /usr/bin/time -f "%E" valgrind ./solver ./tests/deprecated/test_0$i.txt
 done
 
 for i in $(seq 1 5)
 do
   echo "test_0$i.txt"
-  /usr/bin/time -f "%E" ./solver ./tests/test_0$i.txt
+  timeout $TIME_LIMIT /usr/bin/time -f "%E" valgrind ./solver ./tests/test_0$i.txt
 done
 
 for i in $(seq 6 9)
 do
   echo "test_0$i.txt"
-  /usr/bin/time -f "%E" ./solver ./tests/Lunatic/test_0$i.txt
+  timeout $TIME_LIMIT /usr/bin/time -f "%E" valgrind ./solver ./tests/Lunatic/test_0$i.txt
 done
 
